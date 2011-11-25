78e0bc4f2fa1a8ad75a7ac7d05547ff5e9052aab
Marieke van der Tuin <msvandertuin@gmail.com>
2011-11-25 18:08:24 +0100

    Suggests zodanig aangepast dat er slechts 3 genres worden meegenomen met limit 20. Daarnaast koppeling met lokale database artiesten verwijderd. Hiervoor is veld artistname toegevoegd in de tabel artistrating. Zie daarvoor sql bestand in hoofdmap.

--- ike/pages/suggest.php
+++ ike/pages/suggest.php
@@ -1,17 +1,18 @@
 <?php
 useLib('htmlpage');
-useLib('musicbrainz');
+//useLib('musicbrainz');
 
 //generate page header
 fw_header('Suggesties');
 ?>
 <p>U heeft aangegeven muziekgenres leuk te vinden in de volgende volgorde: <?=$_POST['sorted']?></p>
+<p>Dit levert de volgende lijst met aanbevolen artiesten op:</p>
 <?php
 error_reporting(E_ALL);
 // Functie getRating zorgt voor het opvragen van de rating van een bepaalde artiest.
 function getRating($mbid) {
 	// De query voor het opvragen van de rating op MusicBrainz.
-	$r = new HttpRequest('http://musicbrainz.org/ws/1/artist/'.$mbid.'?inc=ratings');
+	$r = new HttpRequest('http://musicbrainz.org/ws/2/artist/'.$mbid.'?inc=ratings');
 	try {
 		$r->send();
 		if ($r->getResponseCode() == 200) {
@@ -32,17 +33,18 @@ $truncate = $db->prepare("TRUNCATE TABLE artistrating");
 $truncate->execute();
 
 // Query opstellen waarbij de artiest id en de rating worden toegevoegd aan de database
-$q=$db->prepare("INSERT INTO artistrating (id,rating) VALUES(:id,:rating)");
+$q=$db->prepare("INSERT INTO artistrating (id,rating,artistname) VALUES(:id,:rating, :artistname)");
 $q->bindParam(':id', $id, PDO::PARAM_STR);
 $q->bindParam(':rating', $rating, PDO::PARAM_INT);
+$q->bindParam(':artistname', $name, PDO::PARAM_STR);
 
 // De array met tags
 $tagbrei = explode(',',$_POST['sorted']);
-$tag = array_slice($tagbrei, 0, 5);
+$tag = array_slice($tagbrei, 0, 3);
 // In deze forloop wordt per genre een query op de musicbrainz dataset gevuurd.
-for($i = 0; $i<5; $i++) {
+for($i = 0; $i<3; $i++) {
 	try {
-		$r = new HttpRequest('http://musicbrainz.org/ws/2/artist/?query=tag:'.$tag[$i]);
+		$r = new HttpRequest('http://musicbrainz.org/ws/2/artist/?query=tag:'.$tag[$i].'&limit=20');
 		$h= $r->getHeaders();
 		$h['User-Agent'] = 'IKE G4 0.1';
 		$r->setHeaders($h);
@@ -54,11 +56,11 @@ for($i = 0; $i<5; $i++) {
 			// Voor elke artiest die hieruit volgt, wordt de rating opgehaald met de getRating functie.
 			foreach($response as $child){
 				$id=$child->attributes()->id;
+				$name=$child->name;
 				$rat=getRating($child->attributes()->id);
 				// De rating wordt vervolgens gewogen.
 				$rating=($rat+0)*(5-$i);
 				// En de artiest - rating koppel in de database opgeslagen.
-				//TODO : dubbele artiesten samen laten voegen!
 				$q->execute();				
 			}
 		}
@@ -67,21 +69,16 @@ for($i = 0; $i<5; $i++) {
 	}
 }
 // Uit de database de 1e 10 rijen gesorteerd op waardering opvragen
-$q=$db->prepare("SELECT id, rating FROM artistrating ORDER BY rating DESC LIMIT 10");
+$q=$db->prepare("SELECT id, rating, artistname FROM artistrating WHERE rating>0 ORDER BY rating DESC LIMIT 10");
 $q->execute();
 $resultArray = $q->fetchAll();
 
-// Vervolgens deze data koppelen met de data van artiesten die in de database zit. 
-$query2= $db->prepare("SELECT * FROM artist WHERE mb_id=:id");
-$query2->bindParam(":id", $id);
-echo '2';
 // Nu middels een forloop het resultaat van de artiesten tonen aan de gebruiker.
+echo "<ul>";
 for ( $j = 0; $j < count($resultArray); $j++) {
-	$id = $resultArray[$j]["id"];
-	$query2->execute();
-	$res = $query2->fetch();
-	echo $res["artist_name"]." : ".$res["mb_id"]."<br/>";
+	echo "<li>".$resultArray[$j]["artistname"]."</li>";
 }
+echo "</ul>";
 }catch(Exception $e){
 	var_dump($e);	
 }
