16384b818a037f818b0a3c52c9e4a0240adb5136
Ruben Visser <ruben@dcm360.nl>
2011-12-09 16:58:30 +0100

    Opbouwen boom met genres, 0.1

---
+++ ike/treebuilder-standalone.php
@@ -0,0 +1,29 @@
+<?php
+set_time_limit(0);
+include('framework/config.php');
+include('framework/basic.php');
+useLib('database');
+dbStart();
+include('framework/genretree.php');
+include('framework/sparqllib.php');
+$root = new GenreNode(0, 'root', null);
+$nodes = array();
+$nodes[] = $root;
+$nodes = array_merge($nodes, $root->getChildren());
+foreach($root->getChildren() as $new){
+	$nodes[$new->getID()] = $new;
+}
+for($i = 0; $i<count($nodes); $i++){
+	$node = $nodes[$i];
+	echo $node->getID().'>'.$node->getName()."\r\n";
+	foreach($node->getChildren() as $child){
+		if($child->getID()>$node->getID()){
+			GenreNode::build($child);
+			foreach($child->getChildren() as $new){
+				$nodes[$new->getID()] = $new;
+			}
+		}
+	}
+}
+
+?>
