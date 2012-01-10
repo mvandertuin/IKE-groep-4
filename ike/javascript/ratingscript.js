f8cd8695e9068c679726d5b4a24ec9e1e0ce6e6e
Olaf Maas <olaf.maas@gmail.com>
2012-01-10 14:27:24 +0100

    Drag drop 3 gebaseerd op de graaf.

--- ike/javascript/ratingscript.js
+++ ike/javascript/ratingscript.js
@@ -1,18 +1,22 @@
 function onl(){
+	//Add handlers to the ratingbuttons
 	$(".pos").click(handle);
 	$(".neg").click(handle);
+	$(".more").click(moar);
+	
+	//Disable handlers on already voted song
 	$(".votedon").off("click");
 	$(".votedon").click(function(){ouralert("U heeft deze rating al uitgebracht op deze plaat, u kunt hem nog wel wijzigen.")});
 	$(".delete").click(del);
 	$("#alertbox").overlay({
-	mask: {
-		color: '#ebecff',
-		loadSpeed: 200,
-		opacity: 0.9
-	},
-	closeOnClick: true,
-	oneInstance: false,
-	load: false
+		mask: {
+			color: '#ebecff',
+			loadSpeed: 200,
+			opacity: 0.9
+		},
+		closeOnClick: true,
+		oneInstance: false,
+		load: false
 	});
 }
 
@@ -21,6 +25,10 @@ function ouralert(infotext){
 	$('#alertbox').css('style', 'block');
 
 	$("#alertbox").data("overlay").load();
+}
+
+function moar(){
+	ouralert("<div class='keuze' id='nieuw'>Iets nieuws</div><div class='keuze' id='hetzelfde'>Meer van hetzelfde</div>")
 }
 
 function handle(event){
@@ -42,7 +50,7 @@ function change(data, event){
 		if($(event.target).hasClass('neg')){
 			$(".pos").filter("."+event.target.id.replace("_\-1", "")).removeClass('votedon').off("click").click(handle);
 		}else{
-			$(".neg").filter("."+event.target.id.replace("_\+1", "")).removeClass('votedon').off("click").click(handle);
+			$(".neg").filter("."+event.target.id.replace("_1", "")).removeClass('votedon').off("click").click(handle);
 		}
 	}
 	$(event.target).addClass("votedon").off("click").click(function(){ouralert("U heeft deze rating al uitgebracht op deze plaat, u kunt hem nog wel wijzigen.")});
